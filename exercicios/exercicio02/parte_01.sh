4d74d4eeab858a4719196c4957e966676b918a4f
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-09 08:01:50 -0300

    Update parte_01.sh
    Correção.

--- exercicios/exercicio02/parte_01.sh
+++ exercicios/exercicio02/parte_01.sh
@@ -1,7 +1,10 @@
+# Correção: 0,75
+
 grep -E '[0-9] A' /home/compartilhado/emailsordenados.txt
 
 grep -E '\bA' /home/compartilhado/emailsordenados.txt
 
 grep -E '\b.br$' /home/compartilhado/emailsordenados.txt
 
+# Não entendi o \B. Seria melhor usar o @
 grep -E '\B[0-9][^0-9 ]' /home/compartilhado/emailsordenados.txt
