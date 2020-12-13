71b84d6aaf7f41528c32c8a6b27c3c5a4ce5e3ca
leticiaSaraiva <leticiachave.saraiva@gmail.com>
2020-12-13 15:27:58 -0300

    Atividade 03

---
+++ atividades/atividade03/logins.sh
@@ -0,0 +1,7 @@
+grep -v 'sshd' /home/compartilhado/auth.log.1 
+
+grep -E 'sshd.*session opened' /home/compartilhado/auth.log.1 
+
+grep -E 'sshd.*Disconnected.*root' /home/compartilhado/auth.log.1 
+
+grep -E 'Dec  4 18.*session opened' /home/compartilhado/auth.log.1 
