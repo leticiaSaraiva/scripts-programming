c58b1a46e6e1c650fb642b9c87a10ec97e056ec9
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-28 10:16:25 -0300

    Correção.

--- atividades/atividade04/atualizarDiretorios.sh
+++ atividades/atividade04/atualizarDiretorios.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Correção: 1,0. Ok, mas tente não usar caminhos completos. Bastava colocar passwd.new
 cp /etc/passwd /home/alunos/leticiasaraiva/leticiasaraivascripts20202/atividades/atividade04/passwd.new
 sed -i 's/\/home\/alunos/\/srv\/students/g' passwd.new
