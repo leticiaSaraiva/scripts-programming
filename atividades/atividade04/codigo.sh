3c43c5b102de30bf4ce21ddb66854e534e2ec193
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-28 10:15:38 -0300

    Correção.

--- atividades/atividade04/codigo.sh
+++ atividades/atividade04/codigo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Correção: 1,0
 sed -i 's/#!\/bin\/python/#!\/usr\/python3/' atividade04.py
 sed -i 's/nota1\|nota2\|notaFinal/\U&/g' atividade04.py
 sed -i '2a\import time' atividade04.py
