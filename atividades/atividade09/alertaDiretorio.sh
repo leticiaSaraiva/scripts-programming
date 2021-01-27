ae90b72253735eec5cb0cdc26e1517dc463c58e8
Letícia Saraiva <leticiachave.saraiva@gmail.com>
2021-01-27 16:51:49 -0300

    Atividade 09

---
+++ atividades/atividade09/alertaDiretorio.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+TEMPO=${1}
+DIR=${2}
+
+QTD_ANT=$(ls ${DIR} | wc -l)
+ls ${DIR} > arquivos.txt
+
+while true
+do
+	QTD_NOVA=$(ls ${DIR} | wc -l)
+	ARQS=""
+	if [ ${QTD_NOVA} -gt ${QTD_ANT} ]
+	then
+		DATA=$(date +"%d-%m-%Y %T")
+		
+		for f in $(ls ${DIR})
+		do
+			grep ${f} arquivos.txt > /dev/null
+			if [ $? -ne 0 ]
+			then
+				ARQS="${ARQS} ${f}"
+			fi
+		done
+		
+		echo "[${DATA}] Alteração! ${QTD_ANT}->${QTD_NOVA}. Adicionados: ${ARQS}" >> dirSensors.log
+		
+		QTD_ANT=${QTD_NOVA}
+		ls ${DIR} > arquivos.txt
+		
+	elif [ ${QTD_NOVA} -lt ${QTD_ANT} ]
+	then
+		DATA=$(date +"%d-%m-%Y %T")
+		
+		for l in $(cat arquivos.txt)
+		do
+			if [ ! $(find ${DIR} -name ${l}) ] > /dev/null
+			then
+				ARQS="${ARQS} ${l}"
+			fi	
+		done
+		
+		echo "[${DATA}] Alteração! ${QTD_ANT}->${QTD_NOVA}. Removidos: ${ARQS}" >> dirSensors.log
+		
+		QTD_ANT=${QTD_NOVA}
+		ls ${DIR} > arquivos.txt
+	fi
+	sleep ${TEMPO}
+done
